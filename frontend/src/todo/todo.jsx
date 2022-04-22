2d2bd471c0fb7667dd38a939611164b6347ad732
Emerson Jahn <emersonajahn@gmail.com>
2022-04-22 12:28:46 -0300

    create todoForm and todoList components

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component {
     
@@ -8,6 +10,8 @@ export default class Todo extends Component {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
