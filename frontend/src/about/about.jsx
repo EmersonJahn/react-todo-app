5e82225eff71cb6ac7e413432534d766adb5aae1
Emerson Jahn <emersonajahn@gmail.com>
2022-04-22 12:24:43 -0300

    create PageHeader component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, nisi ipsum harum, velit sit error eius doloribus numquam voluptatibus aut, excepturi quibusdam. Laudantium necessitatibus quae sunt, molestiae omnis facilis ipsa.</p>
+
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea accusamus harum esse eligendi totam nulla laboriosam, illum accusantium labore facere deleniti mollitia. Consequatur odio, accusamus iure iusto incidunt magni. Ipsam?</p>
+
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fuga harum iure eum ratione pariatur optio blanditiis est autem neque. Doloribus aliquid sit distinctio eligendi odio deserunt minima consequatur natus?</p>
     </div>
 );
